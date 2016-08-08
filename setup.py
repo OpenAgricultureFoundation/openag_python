2a97620f0ed633f2cb30e7430e6caadba1dd6395
Leon Chambers <leon8556@gmail.com>
2016-08-08 16:41:13 -0400

    Added dependencies on platformio and coverage

--- setup.py
+++ setup.py
@@ -8,13 +8,15 @@ setup(
         'click>=6.6',
         'couchdb>=1.0.1',
         'requests>=2.10.0',
-        'voluptuous>=0.8.11'
+        'voluptuous>=0.8.11',
+        'platformio>=2.11.2'
     ],
     extras_require={
         "test": [
             'mock>=2.0.0',
             'httpretty>=0.8.14',
-            'nose>=1.3.7'
+            'nose>=1.3.7',
+            'coverage>=4.2'
         ]
     },
     include_package_data=True,
