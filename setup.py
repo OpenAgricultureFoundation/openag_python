16ee2c66c608611fc3a9ec5f3c2e3de5bbd5493b
Leon Chambers <leon8556@gmail.com>
2016-08-08 16:17:13 -0400

    Added test dependencies to setup.py

--- setup.py
+++ setup.py
@@ -6,10 +6,17 @@ setup(
     py_modules=['openag'],
     install_requires=[
         'click>=6.6',
-        'couchdb-1.0.1',
+        'couchdb>=1.0.1',
         'requests>=2.10.0',
         'voluptuous>=0.8.11'
     ],
+    extras_require={
+        "test": [
+            'mock>=2.0.0',
+            'httpretty>=0.8.14',
+            'nose>=1.3.7'
+        ]
+    },
     include_package_data=True,
     entry_points={
         'console_scripts': [
