eadf7d7800c0160d9c9fe2fd0e43660c6d579d3a
Leon Chambers <leon8556@gmail.com>
2016-08-09 09:40:46 -0400

    Bug fixed in setup.py; Added doc requirements file

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='openag',
     version='0.1',
-    py_modules=['openag'],
+    packages=find_packages(),
     install_requires=[
-        'click>=6.6',
         'couchdb>=1.0.1',
         'requests>=2.10.0',
         'voluptuous>=0.8.11',
