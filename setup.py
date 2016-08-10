198069635518da40ed5c1249f9ace9e0ee99f626
Leon Chambers <leon8556@gmail.com>
2016-08-10 11:54:40 -0400

    Updated setup.py with more metadata

--- setup.py
+++ setup.py
@@ -1,13 +1,34 @@
+import os
+import sys
 from setuptools import setup, find_packages
+
+if sys.version_info < (2,7) or sys.version_info.major > 2:
+    sys.exit('Only Python 2.7 is supported')
+
+readme_path = os.path.join(os.path.dirname(__file__), "README.md")
+with open(readme_path) as f:
+    readme = f.read()
 
 setup(
     name='openag',
     version='0.1',
+    author='Open Agriculture Initiative',
+    description='Core Python package for the OpenAg software stack',
+    long_description=readme,
+    license="GPL",
+    url="https://github.com/OpenAgInitiative/openag_python",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 2.7",
+    ],
     packages=find_packages(),
     install_requires=[
         'couchdb>=1.0.1',
         'requests>=2.10.0',
-        'voluptuous>=0.8.11'
+        'voluptuous>=0.8.11',
+        'click>=5,<6'
     ],
     extras_require={
         "test": [
