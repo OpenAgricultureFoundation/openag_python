ce696dc59927f769e29cf021560262dae6a3b844
Leon Chambers <leon8556@gmail.com>
2016-08-19 16:35:55 -0400

    Added new variable types

--- openag/var_types.py
+++ openag/var_types.py
@@ -20,6 +20,9 @@ AIR_HUMIDITY = EnvVar(
     "A measure of the concentration of water in the air relative to the "
     "maximum concentration at the current temperature", "percent relative"
 )
+AIR_CARBON_DIOXIDE = EnvVar(
+    "air_carbon_dioxide", "The amount of Carbon Dioxide in the air", "ppm"
+)
 WATER_TEMPERATURE = EnvVar(
     "water_temperature", "Temperature of the air in degrees Celcius",
     "degrees C"
@@ -28,8 +31,16 @@ WATER_POTENTIAL_HYDROGEN = EnvVar(
     "water_potential_hydrogen", "Potential Hydrogen of the water", "pH"
 )
 WATER_ELECTRICAL_CONDUCTIVITY = EnvVar(
-    "water_electrical_conductivity",
-    "Electrical conductivity of the water in uS/cm", "uS/cm"
+    "water_electrical_conductivity", "Electrical conductivity of the water",
+    "uS/cm"
+)
+WATER_OXIDATION_REDUCTION_POTENTIAL = EnvVar(
+    "water_oxidation_reduction_potential",
+    "Oxidation-reduction potential of the water", "mV"
+)
+WATER_DISSOLVED_OXYGEN = EnvVar(
+    "water_dissolved_oxygen", "A measure of the amount of oxygen in the water",
+    "mg/L"
 )
 RECIPE_START = EnvVar("recipe_start", "Represents the start of a recipe")
 RECIPE_END = EnvVar("recipe_end", "Represents the end of a recipe")
