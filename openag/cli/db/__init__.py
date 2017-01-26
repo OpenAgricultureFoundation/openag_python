81a3204f5bcae237401811738b2dbaf6f0c62737
Leon Chambers <leon8556@gmail.com>
2017-01-26 17:06:27 -0500

    Added command to update module types

--- openag/cli/db/__init__.py
+++ openag/cli/db/__init__.py
@@ -2,11 +2,17 @@ import os
 import json
 import time
 import click
+import subprocess
+from os import path
+from shutil import rmtree
+from tempfile import mkdtemp
 from couchdb.http import urljoin
 
 from openag import _design
 from openag.couch import Server, ResourceNotFound
-from openag.db_names import all_dbs
+from openag.utils import make_dir_name_from_url
+from openag.models import FirmwareModuleType
+from openag.db_names import all_dbs, FIRMWARE_MODULE_TYPE
 from .. import utils
 from ..config import config
 from .db_config import generate_config
@@ -147,3 +153,44 @@ def load_fixture(fixture_file):
                     if item == old_item:
                         continue
                 db[item_id] = item
+
+def update_record(obj, temp_folder):
+    if not "repository" in obj:
+        return obj
+    repo = obj["repository"]
+    if repo["type"] != "git":
+        return obj
+    url = repo["url"]
+    branch = repo.get("branch", "master")
+    dir_name = make_dir_name_from_url(url)
+    subprocess.call(
+        ["git", "clone", "-b", branch, url, dir_name], cwd=temp_folder
+    )
+    config_path = path.join(temp_folder, path.join(dir_name, "module.json"))
+    with open(config_path) as f:
+        info = json.load(f)
+    new_obj = dict(obj)
+    new_obj.update(info)
+    return new_obj
+
+@db.command()
+def update_module_types():
+    """
+    Download the repositories for all of the firmware_module_type records and
+    update them using the `module.json` files from the repositories themselves.
+    Currently only works for git repositories.
+    """
+    local_url = config["local_server"]["url"]
+    server = Server(local_url)
+    db = server[FIRMWARE_MODULE_TYPE]
+    temp_folder = mkdtemp()
+    for _id in db:
+        if _id.startswith("_"):
+            continue
+        obj = db[_id]
+        new_obj = update_record(FirmwareModuleType(obj), temp_folder)
+        new_obj["_rev"] = obj["_rev"]
+        if new_obj != obj:
+            print "Updating object"
+            db[_id] = new_obj
+    rmtree(temp_folder)
